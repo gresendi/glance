943e558b498177967ca2df9b1e3a2d8300dfb0f5
Gilberto Resendiz <gresendi@uci.edu>
2021-11-19 00:25:05 -0800

    installed react-bootsrap, sizing homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,43 @@
 
 import './Home.css'
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import './Home.css'
 const Home = () => {
 
 
   return (
-    <>
-        <h1>This is Home</h1>
-    </>
+    <Container fluid className='box'>
+      <Row className='top-row'>
+        <Col sm={4}>
+          let-top
+        </Col>
+        <Col sm={4}>
+          middle-top
+        </Col>
+        <Col sm={4}>
+          right-top
+        </Col>
+      </Row>
+      <Row className='top-center'>
+        
+      </Row>
+      <Row className='center'>
+        
+        <Col md="auto">Clock goes here</Col>
+      </Row>
+      <Row className='bottom-center'>
+        bottom-center
+      </Row>
+      <Row className='bottom-row'>
+        bottom-row
+      </Row>
+
+
+
+
+    </Container>
   )
 }
 
