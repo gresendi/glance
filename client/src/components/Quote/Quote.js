2cce2e626a790143746e4d99d721cedbacae0b7a
Gilberto <gresendi@uci.edu>
2021-11-27 00:35:00 -0800

    added quote util, now centered

--- client/src/components/Quote/Quote.js
+++ client/src/components/Quote/Quote.js
@@ -1,14 +1,17 @@
 import './Quote.css'
-import Col from 'react-bootstrap/Col'
+import QuoteApi  from '../../utils/QuoteAPI/'
 
-const Quote = ({quote}) =>{
+const Quote = () =>{
 
+ 
+  let quote = QuoteApi.getQuote()
 
   return (
-   
-      <p className='quote'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repudiandae eos suscipit, quisquam voluptatum ratione ad! Obcaecati porro repellendus magni adipisci placeat autem numquam culpa, doloremque sapiente error odit labore.</p>
+   <>
+      <p className='quote'>"{quote.text}"</p>
+      <p className='quote'>-{quote.author?quote.author:<></>}</p>
       
-   
+   </>
   )
 }
 export default Quote
